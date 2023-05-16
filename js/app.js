484ddb7ced180f4a8b21aba511c51cc87d658661
bluefish <edycasa45@gmail.com>
2023-05-16 12:53:21 -0500

    Blank input validation and added a small zoom animation to the error message

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ function encrypt() {
     let messageFoundElement = document.getElementById("message-found");
     let messageNotFoundElement = document.getElementById("message-not-found");
     let decryptedTextElement = document.getElementById("decrypt-text");
+    let decryptSection = document.querySelector(".section-decrypt");
     let encryptedText = "";
 
     const encryptKeys = {
@@ -15,50 +16,68 @@ function encrypt() {
         u: "ufat",
     };
 
-    textToEncrypt;
-
-    for (let letter in textToEncrypt) {
-        if (textToEncrypt[letter] in encryptKeys) {
-            encryptedText += encryptKeys[textToEncrypt[letter]];
-        } else {
-            encryptedText += textToEncrypt[letter];
+    if (textToEncrypt != "") {
+        for (let letter in textToEncrypt) {
+            if (textToEncrypt[letter] in encryptKeys) {
+                encryptedText += encryptKeys[textToEncrypt[letter]];
+            } else {
+                encryptedText += textToEncrypt[letter];
+            }
         }
-    }
 
-    messageNotFoundElement.style.display = "none";
-    messageFoundElement.style.display = "flex";
-    decryptedTextElement.innerHTML = encryptedText;
+        messageNotFoundElement.style.display = "none";
+        messageFoundElement.style.display = "flex";
+        decryptedTextElement.innerHTML = encryptedText;
+    } else {
+        messageNotFoundElement.style.display = "";
+        messageFoundElement.style.display = "none";
+        decryptSection.style.transform = "scale(1.1)";
+
+        setTimeout(() => {
+            decryptSection.style.transform = "scale(1)";
+        }, 300);
+    }
 }
 
 function decrypt() {
     let textToDecrypt = document
         .getElementById("encrypt-prompt")
         .value.toLowerCase();
-
-    let decryptedText = textToDecrypt.replace(
-        /ai|enter|imes|ober|ufat/g,
-        (word) => {
-            if (word == "ai") {
-                return "a";
-            } else if (word == "enter") {
-                return "e";
-            } else if (word == "imes") {
-                return "i";
-            } else if (word == "ober") {
-                return "o";
-            } else {
-                return "u";
-            }
-        }
-    );
-
     let messageFoundElement = document.getElementById("message-found");
     let messageNotFoundElement = document.getElementById("message-not-found");
     let decryptedTextElement = document.getElementById("decrypt-text");
+    let decryptSection = document.querySelector(".section-decrypt");
 
-    messageNotFoundElement.style.display = "none";
-    messageFoundElement.style.display = "flex";
-    decryptedTextElement.innerHTML = decryptedText;
+    if (textToDecrypt != "") {
+        let decryptedText = textToDecrypt.replace(
+            /ai|enter|imes|ober|ufat/g,
+            (word) => {
+                if (word == "ai") {
+                    return "a";
+                } else if (word == "enter") {
+                    return "e";
+                } else if (word == "imes") {
+                    return "i";
+                } else if (word == "ober") {
+                    return "o";
+                } else {
+                    return "u";
+                }
+            }
+        );
+
+        messageNotFoundElement.style.display = "none";
+        messageFoundElement.style.display = "flex";
+        decryptedTextElement.innerHTML = decryptedText;
+    } else {
+        messageNotFoundElement.style.display = "";
+        messageFoundElement.style.display = "none";
+        decryptSection.style.transform = "scale(1.1)";
+
+        setTimeout(() => {
+            decryptSection.style.transform = "scale(1)";
+        }, 300);
+    }
 }
 
 function copy() {
@@ -66,9 +85,9 @@ function copy() {
     navigator.clipboard
         .writeText(copyText)
         .then(() => {
-            alert('Correctamente copiado al portapapeles.');
+            alert("Correctamente copiado al portapapeles.");
         })
         .catch((error) => {
-            alert('ERROR: No se pudo copiar el texto al portapapeles.' + error);
+            alert("ERROR: No se pudo copiar el texto al portapapeles." + error);
         });
 }
